f3a15aae3ef7827fbf5d5f41daef2a4410a28e2f
JC Harrington <jc@brandibble.co>
2020-07-30 12:42:59 -0500

    Updates to order window selection component

--- src/components/requestedAt/RequestedAtTimes.js
+++ src/components/requestedAt/RequestedAtTimes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import {
+  serviceTypeNamesMap,
   capitalize,
   makeOrderTimes,
   time24ToDateStr,
@@ -8,14 +9,26 @@ import {
 } from '@open-tender/js'
 import { Button } from '../index'
 
+const makeOrderWindow = (orderTime) => {
+  if (orderTime.start_time === orderTime.end_time) {
+    return `@ ${time24ToDateStr(orderTime.start_time)}`
+  } else {
+    return `from ${time24ToDateStr(orderTime.start_time)} to ${time24ToDateStr(
+      orderTime.end_time
+    )}`
+  }
+}
+
 const RequestedAtTimes = ({
   orderTimes,
+  serviceType,
   revenueCenter,
   requestedAt,
   setRequestedAt,
 }) => {
   const tz = timezoneMap[revenueCenter.timezone]
   const availableTimes = makeOrderTimes(orderTimes, tz)
+  const serviceTypeName = serviceTypeNamesMap[serviceType].toLowerCase()
 
   const handleRequestedAt = (evt, requestedAt) => {
     evt.preventDefault()
@@ -30,7 +43,7 @@ const RequestedAtTimes = ({
           Choose an order time
         </p>
         <p className="modal__subtitle">
-          Please select from the available pickup times below
+          Please select from the available {serviceTypeName} times below
         </p>
       </div>
       <div className="modal__body">
@@ -47,12 +60,12 @@ const RequestedAtTimes = ({
                 }`}
               >
                 <div className="order-time__container">
-                  <div className="order-time__time">
+                  <div className="order-time__time ot-font-size-small">
                     <p className={current ? 'ot-bold ot-color-headings' : ''}>
-                      {capitalize(i.weekday)} @ {time24ToDateStr(i.start_time)}{' '}
+                      {capitalize(i.weekday)} {makeOrderWindow(i)}{' '}
                       {current ? '(current)' : ''}
                     </p>
-                    <p className="order-time__order-by ot-font-size-small">
+                    <p className="order-time__order-by ot-font-size-x-small">
                       {' '}
                       (order by {orderBy})
                     </p>
@@ -75,6 +88,7 @@ const RequestedAtTimes = ({
 RequestedAtTimes.displayName = 'RequestedAtTimes'
 RequestedAtTimes.propTypes = {
   orderTimes: propTypes.array,
+  serviceType: propTypes.string,
   revenueCenter: propTypes.object,
   requestedAt: propTypes.string,
   setRequestedAt: propTypes.func,
