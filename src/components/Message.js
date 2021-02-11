fcb6e5e6040aaecc893cb94dc09363d7e696f8c9
JC Harrington <jc@brandibble.co>
2021-02-11 09:55:21 -0600

    Update approach to padding for Message component to adjust for small messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,8 +2,8 @@ import styled from '@emotion/styled'
 
 const Message = styled('span')`
   display: inline-block;
-  padding: 1em 1em;
   ${({ theme, color = 'secondary', size = 'main' }) => `
+    padding: ${size === 'small' ? '0.75em 1.5rem' : '1em 1em'};
     line-height: ${theme.lineHeight};
     border-radius: ${theme.border.radiusSmall};
     color: ${theme.colors[color]};
