685feac73a23e1d893bd872a47b0f1add2180fcc
JC Harrington <jc@brandibble.co>
2021-02-13 16:08:40 -0600

    Update padding for Message component based on size

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled'
 const Message = styled('span')`
   display: inline-block;
   ${({ theme, color = 'secondary', size = 'main' }) => `
-    padding: ${size === 'small' ? '0.75em 1.5rem' : '1em 1em'};
+    padding: ${size === 'small' ? '0.6em 1.2rem' : '1em 1em'};
     line-height: ${theme.lineHeight};
     border-radius: ${theme.border.radiusSmall};
     color: ${theme.colors[color]};
