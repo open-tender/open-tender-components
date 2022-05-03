e1a3cb19c9529006c28773766f17a0cf7c3ffe98
JC Harrington <jc@brandibble.co>
2022-05-03 08:05:55 -0400

    First pass at customer notification preferences

--- src/components/forms/CommunicationPreferences/CommunicationPreferences.js
+++ src/components/forms/CommunicationPreferences/CommunicationPreferences.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import propTypes from 'prop-types'
 import styled from '@emotion/styled'
-import { Checkbox, Label } from '../../inputs'
-
-// NotificationArea:
-//   type: string
-//   enum: ['ORDER', 'RATING', 'MARKETING']
-
-// NotificationChannel:
-//   type: string
-//   enum: ['EMAIL', 'SMS', 'PUSH']
+import { Checkbox } from '../../inputs'
 
 const orderNotifications = [
   { notification_area: 'ORDER', notification_channel: 'EMAIL', title: 'Email' },
@@ -35,6 +27,20 @@ const marketing = [
   },
 ]
 
+const ratings = [
+  {
+    notification_area: 'RATING',
+    notification_channel: 'EMAIL',
+    title: 'Email',
+  },
+  { notification_area: 'RATING', notification_channel: 'SMS', title: 'SMS' },
+  {
+    notification_area: 'RATING',
+    notification_channel: 'PUSH',
+    title: 'Push Notifications',
+  },
+]
+
 const CommunicationPreferencesView = styled.div``
 
 const CommunicationPreferencesLabel = styled('div')`
@@ -51,56 +57,89 @@ const CommunicationPreferencesLabel = styled('div')`
   }
 `
 
-const CommunicationPreferencesGroup = styled.div`
-  margin: 0 0 3rem;
-
+const CommunicationPreferencesOptions = styled.div`
   & > label {
     margin: 1rem 0 0;
   }
 `
 
-const CommunicationPreferences = ({ prefs, add, remove }) => {
-  const currentPrefs = prefs.map(
-    (pref) => `${pref.notification_area}_${pref.notification_channel}`
+const CommunicationPreferencesGroupView = styled.div`
+  margin: 0 0 3rem;
+`
+
+const CommunicationPreferencesGroup = ({
+  prefs,
+  add,
+  remove,
+  fields,
+  title,
+}) => {
+  return (
+    <CommunicationPreferencesGroupView>
+      <CommunicationPreferencesLabel>{title}</CommunicationPreferencesLabel>
+      <CommunicationPreferencesOptions>
+        {fields.map((field) => {
+          const id = `${field.notification_area}_${field.notification_channel}`
+          const pref = prefs.find(
+            (i) =>
+              i.notification_area === field.notification_area &&
+              i.notification_channel === field.notification_channel
+          )
+          return (
+            <Checkbox
+              key={id}
+              showLabel={true}
+              required={true}
+              id={id}
+              on={pref ? true : false}
+              onChange={
+                pref
+                  ? () => remove(pref.customer_notification_preference_id)
+                  : () =>
+                      add(field.notification_area, field.notification_channel)
+              }
+              description={field.title}
+            />
+          )
+        })}
+      </CommunicationPreferencesOptions>
+    </CommunicationPreferencesGroupView>
   )
+}
+
+CommunicationPreferencesGroup.displayName = 'CommunicationPreferencesGroup'
+CommunicationPreferencesGroup.propTypes = {
+  prefs: propTypes.array,
+  add: propTypes.func,
+  remove: propTypes.func,
+  fields: propTypes.array,
+  title: propTypes.string,
+}
+
+const CommunicationPreferences = ({ prefs, add, remove }) => {
   return (
     <CommunicationPreferencesView>
-      <CommunicationPreferencesLabel>
-        Order Notifications
-      </CommunicationPreferencesLabel>
-      <CommunicationPreferencesGroup>
-        {orderNotifications.map((field) => {
-          const id = `${field.notification_area}_${field.notification_channel}`
-          return (
-            <Checkbox
-              key={id}
-              showLabel={true}
-              required={true}
-              id={id}
-              on={currentPrefs.includes(id)}
-              // onChange={handleChange}
-              description={field.title}
-            />
-          )
-        })}
-      </CommunicationPreferencesGroup>
-      <CommunicationPreferencesLabel>Marketing</CommunicationPreferencesLabel>
-      <CommunicationPreferencesGroup>
-        {marketing.map((field) => {
-          const id = `${field.notification_area}_${field.notification_channel}`
-          return (
-            <Checkbox
-              key={id}
-              showLabel={true}
-              required={true}
-              id={id}
-              on={currentPrefs.includes(id)}
-              // onChange={handleChange}
-              description={field.title}
-            />
-          )
-        })}
-      </CommunicationPreferencesGroup>
+      <CommunicationPreferencesGroup
+        prefs={prefs}
+        add={add}
+        remove={remove}
+        fields={orderNotifications}
+        title="Order Notifications"
+      />
+      <CommunicationPreferencesGroup
+        prefs={prefs}
+        add={add}
+        remove={remove}
+        fields={ratings}
+        title="Order Ratings"
+      />
+      <CommunicationPreferencesGroup
+        prefs={prefs}
+        add={add}
+        remove={remove}
+        fields={marketing}
+        title="Marketing"
+      />
     </CommunicationPreferencesView>
   )
 }
