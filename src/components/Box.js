c2693ce8df58c4250004858f6ed58146e1407d96
JC Harrington <jc@brandibble.co>
2022-07-22 10:40:19 -0400

    Override font colors in cards

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,12 +1,36 @@
 import styled from '@emotion/styled'
 
 const Box = styled.div`
+  label: Card;
   border-style: solid;
   border-width: ${(props) => props.theme.cards.default.borderWidth};
   border-color: ${(props) => props.theme.cards.default.borderColor};
   border-radius: ${(props) => props.theme.cards.default.borderRadius};
   background-color: ${(props) => props.theme.cards.default.bgColor};
   box-shadow: ${(props) => props.theme.cards.default.boxShadow};
+
+  div,
+  p,
+  p:first-of-type,
+  p > span,
+  p > span > span,
+  div > span {
+    ${(props) =>
+      props.theme.cards.default.overrideFontColors
+        ? `color: ${props.theme.cards.default.textColor};`
+        : ''};
+  }
+
+  div.title,
+  p.title,
+  p:first-of-type.title,
+  p > span.title,
+  div > span.title {
+    ${(props) =>
+      props.theme.cards.default.overrideFontColors
+        ? `color: ${props.theme.cards.default.titleColor};`
+        : ''};
+  }
 `
 
 export default Box
